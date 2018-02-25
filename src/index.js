db68dc9a667c7b2055813b47c7bef6645c0acfe5
Alex <djdoktop@gmail.com>
2018-02-25 19:03:38 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
     // your solution
-
     let arr = str.split('');
 
-    if(arr.length%2===0) {
-        for (let i = 0; i < bracketsConfig.length; i++) {
-            if(bracketsConfig[i][0].localeCompare(bracketsConfig[i][1])===0) {
-                while(((arr.lastIndexOf(bracketsConfig[i][0]) - arr.indexOf(bracketsConfig[i][0])) % 2 !== 0)){
-                    arr.splice(arr.indexOf(bracketsConfig[i][0]), 1);
-                    arr.splice(arr.lastIndexOf(bracketsConfig[i][0]), 1);
-                }
-            }else{
-                while ( (((arr.lastIndexOf(bracketsConfig[i][1]) - arr.lastIndexOf(bracketsConfig[i][0])) % 2 !== 0)) &&
-                ((arr.lastIndexOf(bracketsConfig[i][1]) - arr.lastIndexOf(bracketsConfig[i][0])) > 0)) {
-                    arr.splice(arr.lastIndexOf(bracketsConfig[i][0]), 1);
-                    arr.splice(arr.lastIndexOf(bracketsConfig[i][1]), 1);
+    for(let j=1;j<arr.length;j++){
+        for(let k=0;k<bracketsConfig.length;k++){
+            if(arr.length > 0) {
+                if ((arr[j].localeCompare(bracketsConfig[k][1]) === 0) && (j !== 0) && (arr[j - 1].localeCompare(bracketsConfig[k][0]) === 0)) {
+                    arr.splice(j - 1, 2);
+                    j>1 ? j-=2 : j--;
                 }
             }
-            let flag = true;
-                while ((flag) && (arr.length!==0)) {
-                    if ((arr[0].localeCompare(bracketsConfig[i][0]) === 0) && (arr[arr.length - 1].localeCompare(bracketsConfig[i][1]) === 0)) {
-                        arr.pop();
-                        arr.shift();
-                        console.log('cut ends');
-                        console.log(arr.length);
-                    } else {
-                        flag = false
-                    }
-                }
         }
     }
 
     return arr.length === 0;
-
 };
-//else if((arr.indexOf(bracketsConfig[i][0])!== -1) || (arr.indexOf(bracketsConfig[i][1])!== -1)){
